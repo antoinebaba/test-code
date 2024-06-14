b2acdef5d49906b1c2edf6e9b33aec117d4fff50
Koffi Agbewonou Antoine BABA <antoinebaba5@gmail.com>
2024-06-14 16:57:53 +0300

    React 'hello world'

--- react1/first-app/src/App.js
+++ react1/first-app/src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Item from './myItem'
+
+
 
 function App() {
   return (
@@ -9,6 +13,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Item/>
         <a
           className="App-link"
           href="https://reactjs.org"
